9afb98daa1fa0b500463778afba82594cc118c67
surya <suryapraharsha@gmail.com>
2019-05-24 20:18:08 -0400

    done with files

--- routes/admin.js
+++ routes/admin.js
@@ -12,7 +12,12 @@ router.get('/add-product', adminController.getAddProduct);
 // /admin/products => GET
 router.get('/products', adminController.getProducts);
 
+// /admin/editProduct ==>GET
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product',adminController.postEditProduct);
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
+router.post('/delete-product',adminController.postDeleteProduct);
 module.exports = router;
